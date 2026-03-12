55ebd1f97d0c3e3941f5763b6a723fecc9b0167d
datnv06 <nguyenvietdat36@gmail.com>
2026-03-12 15:42:59 +0700

    lab2

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import "antd/dist/reset.css";
 import "./index.css";
 import App from "./App";
+import Lab2 from "./pages/lab2";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Lab2 />
     </BrowserRouter>
   </StrictMode>
 );
