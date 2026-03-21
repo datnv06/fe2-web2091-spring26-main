058fb208c596b2dfdc44ff33571158892e1a6ef5
datnv06 <nguyenvietdat36@gmail.com>
2026-03-21 15:51:10 +0700

    lab6

--- src/main.tsx
+++ src/main.tsx
@@ -4,26 +4,30 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "antd/dist/reset.css";
 import "./index.css";
+
 import App from "./App";
 import Add from "./lab3/Add";
 import Login from "./lab3/Login";
 import Register from "./lab3/Register";
-import StoryForm from "./pages/lab4";
+import CategoryForm from "./pages/lab4";
 import { StoryList } from "./pages/lab5";
+import EditStory from "./pages/lab6";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-    <QueryClientProvider client={queryClient}>
-      <Routes>
-        <Route path="/" element = {<App></App>}></Route>
-        <Route path="/list" element = {<StoryList></StoryList>}></Route>
-        <Route path="/add" element = {<Add></Add>}></Route>
-        <Route path="/login" element = {<Login></Login>}></Route>
-        <Route path="/register" element = {<Register></Register>}></Route>
-      </Routes>
+      <QueryClientProvider client={queryClient}>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/list" element={<StoryList />} />
+          <Route path="/add" element={<Add />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/category" element={<CategoryForm />} />
+          <Route path="/edit/:id" element={<EditStory />} />
+        </Routes>
       </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>
